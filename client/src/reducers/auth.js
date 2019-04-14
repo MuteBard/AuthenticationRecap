e0977bcd508afe69b2c6e94938db8024725c809a
MuteBard <csevere@brandeis.edu>
2019-04-13 20:53:17 -0400

    wired up action from action creator with reducer as well as its payload

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,10 +1,17 @@
+import { AUTH_USER } from '../action/types'
 
-const INITAL_STATE = {
+ const INITAL_STATE = {
     authenicated: '',
     errorMessage: ''
 }
 
-
 export default function(state = INITAL_STATE, action){
-    return state;
+    switch (action.type){
+        case AUTH_USER:
+            return {...state, authenticated : action.payload}
+        default:
+            return state;
+        }
+    
+    
 }
