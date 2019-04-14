81c3c1aad5d16fbf70dcebe16890865360971bb4
MuteBard <csevere@brandeis.edu>
2019-04-13 22:07:30 -0400

    able to signout with new Signout.js with a new action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,3 +11,13 @@ export const signup = (formProps, callback) => async dispatch  => {
         dispatch({ type: AUTH_ERROR, payload: `${formProps.email} is in use` })
     }
 }
+
+
+export const signout = () => {
+    localStorage.removeItem('token')
+
+    return{
+        type : AUTH_USER,
+        payload : '' 
+    }
+}
